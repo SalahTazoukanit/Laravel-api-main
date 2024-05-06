40127e884374348f3be432342a533c0c0c0e37b6
SalahTazoukanit <salahtazu1996@gmail.com>
2024-05-06 10:34:03 +0200

    product model swagger

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,6 +7,61 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * @OA\Schema(
+ *     title="Product",
+ *     description="Product model",
+ *     @OA\Property(
+ *         property="id",
+ *         type="integer",
+ *         description="Product ID"
+ *     ),
+ *     @OA\Property(
+ *         property="name",
+ *         type="string",
+ *         description="Product name"
+ *     ),
+ *     @OA\Property(
+ *         property="price",
+ *         type="number",
+ *         format="float",
+ *         description="Product price"
+ *     ),
+ *     @OA\Property(
+ *         property="stock",
+ *         type="integer",
+ *         description="Product stock"
+ *     ),
+ *     @OA\Property(
+ *         property="description",
+ *         type="string",
+ *         description="Product description"
+ *     ),
+ *     @OA\Property(
+ *         property="image",
+ *         type="string",
+ *         description="URL of the product image"
+ *     ),
+ *     @OA\Property(
+ *         property="categories",
+ *         type="array",
+ *         @OA\Items(ref="#/components/schemas/Categorie")
+ *     ),
+ *     @OA\Property(
+ *         property="created_at",
+ *         type="string",
+ *         format="date-time",
+ *         description="Date and time when the product was created"
+ *     ),
+ *     @OA\Property(
+ *         property="updated_at",
+ *         type="string",
+ *         format="date-time",
+ *         description="Date and time when the product was last updated"
+ *     )
+ * )
+ */
+
 class Product extends Model
 {
     use HasFactory;
