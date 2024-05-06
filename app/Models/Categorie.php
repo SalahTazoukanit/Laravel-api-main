9cac6bf3f8dd79c871ca90cbc770d352afeb0b8a
SalahTazoukanit <salahtazu1996@gmail.com>
2024-05-06 10:35:17 +0200

    categorie model swagger

--- app/Models/Categorie.php
+++ app/Models/Categorie.php
@@ -6,7 +6,44 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
-
+/**
+ * @OA\Schema(
+ *     title="Categorie",
+ *     description="Categorie model",
+ *     @OA\Property(
+ *         property="id",
+ *         type="integer",
+ *         description="Categorie ID"
+ *     ),
+ *     @OA\Property(
+ *         property="name",
+ *         type="string",
+ *         description="Categorie name"
+ *     ),
+ *     @OA\Property(
+ *         property="description",
+ *         type="string",
+ *         description="Categorie description"
+ *     ),
+ *     @OA\Property(
+ *         property="products",
+ *         type="array",
+ *         @OA\Items(ref="#/components/schemas/Product")
+ *     ),
+ *     @OA\Property(
+ *         property="created_at",
+ *         type="string",
+ *         format="date-time",
+ *         description="Date and time when the categorie was created"
+ *     ),
+ *     @OA\Property(
+ *         property="updated_at",
+ *         type="string",
+ *         format="date-time",
+ *         description="Date and time when the categorie was last updated"
+ *     )
+ * )
+ */
 class Categorie extends Model
 {
     use HasFactory;
